61a5c7e815b9460634c5d3c749abe04baf0a3c9a
teemuatlut <teemu.mantykallio@live.fi>
2018-03-02 20:22:53 +0200

    Update examples

--- examples/TMC2208_Simple/TMC2208_Simple.ino
+++ examples/TMC2208_Simple/TMC2208_Simple.ino
@@ -1,30 +1,38 @@
 // Author Teemu Mäntykallio, 2017-04-07
 
 // Define pins
-#define EN_PIN    13 															// LOW: Driver enabled. HIGH: Driver disabled
-#define STEP_PIN  54 															// Step on rising edge
+#define EN_PIN    38  // LOW: Driver enabled. HIGH: Driver disabled
+#define STEP_PIN  54  // Step on rising edge
 
-#include <TMC2208Stepper.h>												// Include library
-TMC2208Stepper driver = TMC2208Stepper(&Serial);	// Create driver and use
-																									// HardwareSerial0 for communication
+#include <TMC2208Stepper.h>                       // Include library
+TMC2208Stepper driver = TMC2208Stepper(Serial1);  // Create driver and use
+                                                  // HardwareSerial0 for communication
 
 void setup() {
-	Serial.begin(115200);														// Init used serial port
-	while(!Serial);																	// Wait for port to be ready
+  Serial.begin(9600);
+  Serial.println("Start...");
+  Serial1.begin(115200);        // Start hardware serial 1
+  driver.push();                // Reset registers
 
-	// Prepare pins
-	pinMode(EN_PIN, OUTPUT);
-	pinMode(STEP_PIN, OUTPUT);
+  // Prepare pins
+  pinMode(EN_PIN, OUTPUT);
+  pinMode(STEP_PIN, OUTPUT);
+  digitalWrite(EN_PIN, HIGH);   // Disable driver in hardware
 
-	driver.pdn_disable(1);													// Use PDN/UART pin for communication
-	driver.I_scale_analog(0);												// Adjust current from the registers
-	driver.rms_current(500);												// Set driver current 500mA
-	driver.toff(0x2);																// Enable driver
+  driver.pdn_disable(true);     // Use PDN/UART pin for communication
+  driver.I_scale_analog(false); // Use internal voltage reference
+  driver.rms_current(500);      // Set driver current 500mA
+  driver.toff(2);               // Enable driver in software
 
-	digitalWrite(13, LOW);													// Enable driver
+  digitalWrite(EN_PIN, LOW);    // Enable driver in hardware
+
+  uint32_t data = 0;
+  Serial.print("DRV_STATUS = 0x");
+  driver.DRV_STATUS(&data);
+  Serial.println(data, HEX);
 }
 
 void loop() {
-	digitalWrite(STEP_PIN, !digitalRead(STEP_PIN)); // Step
-	delay(10);
+  digitalWrite(STEP_PIN, !digitalRead(STEP_PIN));
+  delayMicroseconds(100);
 }
--- examples/TMC2208_SoftwareSerial/TMC2208_SoftwareSerial.ino
+++ examples/TMC2208_SoftwareSerial/TMC2208_SoftwareSerial.ino
@@ -1,35 +1,33 @@
 // Author Teemu Mäntykallio, 2017-04-07
 
 // Define pins
-#define EN_PIN    13 															// LOW: Driver enabled. HIGH: Driver disabled
-#define DIR_PIN   11 															// Set stepping direction
-#define STEP_PIN  54 															// Step on rising edge
-#define TX_PIN 	  52															// SoftwareSerial
-#define RX_PIN    50															// 
+#define EN_PIN    38  // LOW: Driver enabled. HIGH: Driver disabled
+#define STEP_PIN  54  // Step on rising edge
+#define RX_PIN    63  // SoftwareSerial pins
+#define TX_PIN    40  //
 
-#include <SoftwareSerial.h>
-SoftwareSerial tmc_sw = SoftwareSerial(RX_PIN, TX_PIN);
+#include <TMC2208Stepper.h>
 
-#include <TMC2208Stepper.h>												// Include library
-TMC2208Stepper driver = TMC2208Stepper(&tmc_sw);	// Create driver that uses SoftwareSerial for communication
+// Create driver that uses SoftwareSerial for communication
+TMC2208Stepper driver = TMC2208Stepper(RX_PIN, TX_PIN);
 
 void setup() {
-	tmc_sw.begin(115200);														// Init used serial port
-	while(!tmc_sw);																	// Wait for port to be ready
+  driver.beginSerial(115200);
+  // Push at the start of setting up the driver resets the register to default
+  driver.push();
+  // Prepare pins
+  pinMode(EN_PIN, OUTPUT);
+  pinMode(STEP_PIN, OUTPUT);
 
-	// Prepare pins
-	pinMode(EN_PIN, OUTPUT);
-	pinMode(STEP_PIN, OUTPUT);
-	pinMode(DIR_PIN, OUTPUT);
+  driver.pdn_disable(true);     // Use PDN/UART pin for communication
+  driver.I_scale_analog(false); // Use internal voltage reference
+  driver.rms_current(500);      // Set driver current = 500mA, 0.5 multiplier for hold current and RSENSE = 0.11.
+  driver.toff(2);               // Enable driver in software
 
-	driver.pdn_disable(1);													// Use PDN/UART pin for communication
-	driver.rms_current(500, 0.5, 0.11);							// Set driver current = 500mA, 0.5 multiplier for hold current and RSENSE = 0.11.
-	driver.toff(0x2);																// Enable driver
-
-	digitalWrite(13, LOW);													// Enable driver
+  digitalWrite(EN_PIN, LOW);    // Enable driver in hardware
 }
 
 void loop() {
-	digitalWrite(STEP_PIN, !digitalRead(STEP_PIN)); // Step
-	delay(10);
+  digitalWrite(STEP_PIN, !digitalRead(STEP_PIN));
+  delay(1);
 }
